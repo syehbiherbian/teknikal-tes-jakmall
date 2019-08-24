a3c849a6898817afc24c8fa2bb1d4e147a022722
Syehbi Herbian <syehbiherbian@gmail.com>
2019-08-24 16:55:04 +0700

    Add Feature Substract

--- commands.php
+++ commands.php
@@ -3,4 +3,6 @@
 return [
     // TODO : Add list of commands here
     \Jakmall\Recruitment\Calculator\Commands\CommandAdd::class,
+    \Jakmall\Recruitment\Calculator\Commands\CommandSubstract::class,
+
 ];
