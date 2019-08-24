bd7cfab60fcf3938cf7cff8ef246d1c24a00dc6d
Syehbi Herbian <syehbiherbian@gmail.com>
2019-08-24 18:39:26 +0700

    add feature multiply

--- commands.php
+++ commands.php
@@ -4,5 +4,6 @@ return [
     // TODO : Add list of commands here
     \Jakmall\Recruitment\Calculator\Commands\CommandAdd::class,
     \Jakmall\Recruitment\Calculator\Commands\CommandSubstract::class,
+    \Jakmall\Recruitment\Calculator\Commands\CommandMultiply::class,
 
 ];
